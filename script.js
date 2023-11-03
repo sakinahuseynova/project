9e5ab5129fd39ad723b83ec6c181187d7e69b702
sakinahuseynova <huseynovasekine@gmail.com>
2023-11-03 23:24:43 +0400

    <sss>

---
+++ script.js
@@ -0,0 +1,7 @@
+const myButton = document.querySelector('.book-room');
+const booking = document.querySelector('.booking')
+
+myButton.addEventListener ('click', ()=> {
+    booking.style.display = "block";
+
+})
